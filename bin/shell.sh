972a75b0880ef5bee0ffa4e0d3a8309d6b94e3ed
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-10-12 11:54:37 +0900

    shell.shでprofile と region指定できるように修正

--- bin/shell.sh
+++ bin/shell.sh
@@ -12,6 +12,10 @@ DBログインコマンド
    ヘルプを表示
  -e | --env-file <ENV_PATH>: (required)
    環境変数ファイルを指定
+ --profile <AWS_PROFILE>:
+   awsのプロファイル名を指定 (default=default)
+ --region <AWS_REGION>:
+   awsのリージョンを指定 (default=ap-northeast-1)
 EOS
 exit 1
 }
@@ -22,11 +26,15 @@ source "${SCRIPT_DIR}/lib/utils.sh"
 
 APP_NAME=$(get_app_name ${PROJECT_ROOT}/app_name)
 
+AWS_PROFILE="default"
+AWS_REGION="ap-northeast-1"
 args=()
 while [ "$#" != 0 ]; do
   case $1 in
     -h | --help     ) usage;;
     -e | --env-file ) shift;ENV_PATH="$1";;
+    --profile       ) shift;AWS_PROFILE="$1";;
+    --region        ) shift;AWS_REGION="$1";;
     -* | --*        ) error "$1 : 不正なオプションです" ;;
     *               ) args+=("$1");;
   esac
@@ -40,9 +48,14 @@ done
 env_tmp="$(mktemp)"
 cat "$ENV_PATH" > "$env_tmp"
 
+AWS_ACCESS_KEY_ID=$(aws --profile $AWS_PROFILE --region $AWS_REGION configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws --profile $AWS_PROFILE --region $AWS_REGION configure get aws_secret_access_key)
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> "$env_tmp"
+echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> "$env_tmp"
+
 set -e
 trap 'rm $env_tmp;' EXIT
-
+cat $env_tmp
 docker run --rm -ti \
   --network host \
   --env-file "$env_tmp" \
